f107f9c45207180075f237b0f7fa9e2a027a1c8d
Isaac Chen <isaacchen@isaacchen.cn>
2018-07-13 05:00:59 +0200

    land: Import XiaomiDoze from msm8996-common
    Change-Id: Id2ad90466f64761a43bb8c768630b14620bee35c
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- device.mk
+++ device.mk
@@ -189,6 +189,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Updates
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
