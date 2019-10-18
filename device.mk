afbbf8cd147cb37b44c0d6fda64d54ac8a65d4f1
weritos666 <weritos94@gmail.com>
2019-10-18 11:03:36 +0000

    land:USB Basic

--- device.mk
+++ device.mk
@@ -397,7 +397,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.land
+    android.hardware.usb@1.0-service.basic
 
 # Thermal
 PRODUCT_PACKAGES += \
