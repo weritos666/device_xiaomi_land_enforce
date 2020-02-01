62e191ce2f88f5ad9c3003cefc39962318e37f89
weritos666 <weritos94@gmail.com>
2020-02-01 20:23:02 +0000

    land:Let's fix the paths for Selinux

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -238,8 +238,8 @@ TARGET_RECOVERY_FSTAB 		 := $(DEVICE_PATH)/rootdir/fstab.qcom
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 SELINUX_IGNORE_NEVERALLOWS := true
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 #Treble
 ENABLE_VENDOR_IMAGE := true
