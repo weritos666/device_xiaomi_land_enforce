24129f398d23c8b353d3267f32e7f81fab79e0de
weritos666 <weritos94@gmail.com>
2020-02-02 10:37:13 +0000

    Revert "land: biometrics: Build fingerprint HAL 2.1"
    This reverts commit f0669b9ceb55ea5580ca8414034857dfbafbf982.

--- device.mk
+++ device.mk
@@ -200,7 +200,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.land
+    android.hardware.biometrics.fingerprint@2.1-service.custom
 
 # FM
 PRODUCT_PACKAGES += \
