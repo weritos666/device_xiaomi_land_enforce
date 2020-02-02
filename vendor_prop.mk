33a0d21c6c84a8400c0715e25321024bb0f5fa2e
JacobDeepu <d2jdeepu@gmail.com>
2020-02-02 21:53:41 +0530

    land: Remove prop ro.fingerprint.cleanup.unused

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -99,8 +99,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.qfp=false \
-    ro.fingerprint.cleanup.unused=false
+    persist.qfp=false
 
 # FM
 PRODUCT_PROPERTY_OVERRIDES += \
