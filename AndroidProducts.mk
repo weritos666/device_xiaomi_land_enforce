ef143f19f669961a13aca19e4c5fbf03430c65a5
weritos666 <weritos94@gmail.com>
2020-02-04 13:02:01 +0000

    land:Havoc BringUP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_land.mk
+    $(LOCAL_DIR)/havoc_land.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_land-user \
-    lineage_land-userdebug \
-    lineage_land-eng
+    havoc_land-user \
+    havoc_land-userdebug \
+    havoc_land-eng
